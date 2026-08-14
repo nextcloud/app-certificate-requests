d320e5f2944a0c2a42725e7defe0e3f40183b666
zomtec2311 <wtoedt@gmail.com>
2026-08-14 15:40:57 +0200

    Update

---
+++ l10n/de_DE.js
@@ -0,0 +1,12 @@
+OC.L10N.register(
+    "compact_calendar_widget",
+    {
+    "Upcoming events" : "Anstehende Termine",
+    "Week" : "Woche",
+    "Month" : "Monat",
+    "Year" : "Jahr",
+    "Back" : "Zurück",
+    "Forward" : "Vor",
+    "Current" : "Aktuell"
+},
+"nplurals=2; plural=(n != 1);");
